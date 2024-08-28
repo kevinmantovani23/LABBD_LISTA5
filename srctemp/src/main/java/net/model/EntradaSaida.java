25506bc306d6c68f925a0f404a10f04b26fb0026
Kevin Mantovani <122639072+kevinmantovani23@users.noreply.github.com>
2024-08-28 17:33:57 -0300

    Add files via upload

---
+++ srctemp/src/main/java/net/model/EntradaSaida.java
@@ -0,0 +1,44 @@
+package net.model;
+
+public class EntradaSaida {
+	
+		
+	private int codigo_transacao;
+	private int codigo_produto;
+	private int quantidade;
+	private String tipo;
+	private long valor_total;
+	
+	public int getCodigo_transacao() {
+		return codigo_transacao;
+	}
+	public void setCodigo_transacao(int codigo_transacao) {
+		this.codigo_transacao = codigo_transacao;
+	}
+	public int getCodigo_produto() {
+		return codigo_produto;
+	}
+	public void setCodigo_produto(int codigo_produto) {
+		this.codigo_produto = codigo_produto;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public long getValor_total() {
+		return valor_total;
+	}
+	public void setValor_total(long valor_total) {
+		this.valor_total = valor_total;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+}
---
+++ srctemp/src/main/java/net/model/Produto.java
@@ -0,0 +1,29 @@
+package net.model;
+
+public class Produto {
+	
+	private int codigo;
+	private String nome;
+	private long valor;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public long getValor() {
+		return valor;
+	}
+	public void setValor(long valor) {
+		this.valor = valor;
+	}
+	
+}
+
